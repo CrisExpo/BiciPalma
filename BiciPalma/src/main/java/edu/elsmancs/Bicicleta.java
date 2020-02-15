f83d15f421faca1b11f49fa12c36fd7b77977614
CrisExpositoSerra <crisexposito18@gmail.com>
2020-02-15 15:53:55 +0100

    feat(Bicipalma):    añadidio constructor

--- BiciPalma/src/main/java/edu/elsmancs/Bicicleta.java
+++ BiciPalma/src/main/java/edu/elsmancs/Bicicleta.java
@@ -5,10 +5,12 @@ public class Bicicleta {
     private final int id;
 
     public Bicicleta(int id) {
+
         this.id = id;
     }
 
     public int getId() {
+
         return this.id;
     }
 }
